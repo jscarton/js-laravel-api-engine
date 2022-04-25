abcaa49d010b12b61f9094b07c220139e4c9fac1
Juan Scarton <jscarton@gmail.com>
2022-04-25 13:51:39 -0300

    initial support for spanish language

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Estas credenciales no coinciden con nuestros registros.',
+    'password' => 'La contraseña provista es incorrecta.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+	'throttle' => 'Demasiados intentos de autenticación. Por favor intente de nuevo en :seconds segundos.',
+
+];
